5b28ac3d5a8df1010db6c6a0c20cac28dc3077a3
Glitch (cave-in) <none>
2018-04-03 03:20:58 +0000

    👢🐒 Checkpoint ./src/client/settings.ts:97325/3525 ./src/client/main.ts:97325/2604

--- src/client/main.ts
+++ src/client/main.ts
@@ -1,10 +1,9 @@
 /// main.ts
 //Define the different pages and how they interact
 
-import { Howler, Howl } from 'howler';
-
 import * as Pages from './pages';
 import * as Game from './wrapper';
+import { initialize as initSettings } from './settings';
 
 function showSingle(select: string, except: string) {
   document.querySelectorAll(select).forEach(e => {
@@ -88,104 +87,4 @@ Pages.registerPage(new Pages.Page({
 }));
 
 Pages.initialize();
-
-// Background music setup
-
-const audioMusic = new Howl({ preload: false, src: ['/assets/cavein.wav'] });
-let audioMusicId: number | null = null;
-
-audioMusic.on('end', () => {
-  audioMusicId = audioMusic.play();
-}).on('play', () => {
-  document.body.classList.add('music-enabled');
-}).on('pause', () => {
-  document.body.classList.remove('music-enabled');
-});
-
-function music(enable?: boolean) {
-  if (enable === undefined) {
-    enable = !localStorage.getItem('no-music');
-  } else if (enable) {
-    localStorage.removeItem('no-music');
-  } else {
-    localStorage.setItem('no-music', 'true');
-  }
-  
-  //Make sure the page is visible
-  if (document.hidden) {
-    enable = false;
-  }
-  
-  if (audioMusicId === null) {
-    if (enable) {
-      audioMusicId = audioMusic.play();
-    }
-  } else if (enable) {
-    audioMusic.play(audioMusicId);
-  } else {
-    audioMusic.pause(audioMusicId);
-  }
-};
-
-window.addEventListener('visibilitychange', () => {
-  Howler.mute(document.hidden);
-  music();
-});
-
-audioMusic.once('load', () => {
-  console.log('music loaded');
-  document.body.classList.add('music-loaded');
-  music();
-}).load();
-
-// Fullscreen toggling
-
-function fullscreenEnter() {
-  const element: any = document.documentElement;
-  const names = [
-    'requestFullscreen',
-    'webkitRequestFullscreen',
-    'mozRequestFullScreen',
-    'msRequestFullscreen'
-  ];
-  for (let i = 0; i < names.length; ++i) {
-    if (element[names[i]]) {
-      element[names[i]]();
-      break;
-    }
-  }
-  (window.screen as any).lock('landscape');
-};
-
-function fullscreenExit() {
-  const element: any = document;
-  const names = [
-    'exitFullscreen',
-    'webkitExitFullscreen',
-    'mozCancelFullScreen',
-    'msExitFullscreen'
-  ];
-  for (let i = 0; i < names.length; ++i) {
-    if (element[names[i]]) {
-      element[names[i]]();
-      break;
-    }
-  }
-};
-
-// Attach event listeners
-
-const listeners: { [key: string]: () => void } = {
-  'enable-music': () => music(true),
-  'disable-music': () => music(false),
-  'enter-fullscreen': () => fullscreenEnter(),
-  'exit-fullscreen': () => fullscreenExit(),
-};
-document.addEventListener('click', (evt) => {
-  Object.entries(listeners).forEach(([key, func]) => {
-    const target = evt.target as HTMLElement;
-    if (target.closest('a[data-onclick-' + key + ']')) {
-      func();
-    }
-  });
-});
+initSettings();
---
+++ src/client/settings.ts
@@ -0,0 +1,105 @@
+/// settings.ts
+//Fullscreen toggle, plus music and audio handling
+
+import { Howler, Howl } from 'howler';
+
+const audioMusic = new Howl({ preload: false, src: ['/assets/cavein.wav'] });
+let audioMusicId: number | null = null;
+
+audioMusic.on('end', () => {
+  audioMusicId = audioMusic.play();
+}).on('play', () => {
+  document.body.classList.add('music-enabled');
+}).on('pause', () => {
+  document.body.classList.remove('music-enabled');
+});
+
+export function music(enable?: boolean) {
+  if (enable === undefined) {
+    enable = !localStorage.getItem('no-music');
+  } else if (enable) {
+    localStorage.removeItem('no-music');
+  } else {
+    localStorage.setItem('no-music', 'true');
+  }
+  
+  //Make sure the page is visible
+  if (document.hidden) {
+    enable = false;
+  }
+  
+  if (audioMusicId === null) {
+    if (enable) {
+      audioMusicId = audioMusic.play();
+    }
+  } else if (enable) {
+    audioMusic.play(audioMusicId);
+  } else {
+    audioMusic.pause(audioMusicId);
+  }
+}
+
+// Fullscreen toggling
+
+export function fullscreenEnter() {
+  const element: any = document.documentElement;
+  const names = [
+    'requestFullscreen',
+    'webkitRequestFullscreen',
+    'mozRequestFullScreen',
+    'msRequestFullscreen'
+  ];
+  for (let i = 0; i < names.length; ++i) {
+    if (element[names[i]]) {
+      element[names[i]]();
+      break;
+    }
+  }
+  (window.screen as any).lock('landscape');
+};
+
+export function fullscreenExit() {
+  const element: any = document;
+  const names = [
+    'exitFullscreen',
+    'webkitExitFullscreen',
+    'mozCancelFullScreen',
+    'msExitFullscreen'
+  ];
+  for (let i = 0; i < names.length; ++i) {
+    if (element[names[i]]) {
+      element[names[i]]();
+      break;
+    }
+  }
+};
+
+// Attach event listeners
+
+export function initialize() {
+  window.addEventListener('visibilitychange', () => {
+    Howler.mute(document.hidden);
+    music();
+  });
+  
+  audioMusic.once('load', () => {
+    console.log('music loaded');
+    document.body.classList.add('music-loaded');
+    music();
+  }).load();
+  
+  const listeners: { [key: string]: () => void } = {
+    'enable-music': () => music(true),
+    'disable-music': () => music(false),
+    'enter-fullscreen': () => fullscreenEnter(),
+    'exit-fullscreen': () => fullscreenExit(),
+  };
+  document.addEventListener('click', (evt) => {
+    Object.entries(listeners).forEach(([key, func]) => {
+      const target = evt.target as HTMLElement;
+      if (target.closest('a[data-onclick-' + key + ']')) {
+        func();
+      }
+    });
+  });
+}
