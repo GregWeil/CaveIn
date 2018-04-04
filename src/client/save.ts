e69feb36d87aaa0a2414b43e2c27d6ab0fbfc22c
Glitch (cave-in) <none>
2018-04-04 01:10:41 +0000

    🚕🏫 Checkpoint ./src/client/save.ts:97325/203

--- src/client/save.ts
+++ src/client/save.ts
@@ -3,3 +3,10 @@
 
 import * as Replay from '../game/replay';
 
+let best: any | null | undefined = undefined;
+export async function getBest() {
+  if (best !== undefined) {
+    return best;
+  }
+  
+}
