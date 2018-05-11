5fa52e5144514091366405b5dee82c6f1b729dcb
Glitch (cave-in) <none>
2018-05-11 03:30:40 +0000

    🐏🌄 Checkpoint ./src/game/collide.ts:97325/3

--- src/game/collide.ts
+++ src/game/collide.ts
@@ -85,10 +85,10 @@ export default class Collide extends BaseObject {
     return null;
   }
   
-  get(pos: Vector2, config: { type: { new(): BaseObject }, ignore: BaseObject[] } = {}) {
+  get(pos: Vector2, config: { type?: { new(): BaseObject }, ignore?: BaseObject[] } = {}) {
     config = Object.assign({ ignore: [] }, config);
     const item = this.getData(pos.hash()).find(item => {
-      if (config.ignore.includes(item.instance)) return false;
+      if (config.ignore!.includes(item.instance)) return false;
       if (config.type && !(item.instance instanceof config.type)) return false;
       return true;
     });
