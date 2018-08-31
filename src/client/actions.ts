faa3b9a10481cb1ca47d56bb62cbd811ec8adea1
Glitch (cave-in) <none>
2018-08-31 14:06:28 +0000

    🎄🛳 Checkpoint ./src/client/actions.ts:97325/873

--- src/client/actions.ts
+++ src/client/actions.ts
@@ -7,24 +7,44 @@ import Replay from '../game/replay';
 export interface State {
   page: string;
   fullscreen: boolean;
-  save: Replay|null|undefined;
-  best: Replay|null|undefined;
+  save: Replay|null;
+  saveValidated: boolean;
+  best: Replay|null;
+  bestValidated: boolean;
 };
 
 export interface Actions {
   setPage: (page: string) => ActionResult<State>;
   setFullscreen: (fullscren: boolean) => ActionResult<State>;
-  loadSave: (save: Replay|null) => ActionResult<State>;
-  loadBest: (save: Replay|null) => ActionResult<State>;
   clearSave: (save: Replay|null) => ActionResult<State>;
   saveReplay: (save: Replay|null) => ActionResult<State>;
 };
 
+function writeToStorage(name: string, replay: Replay|null) {
+  if (replay) {
+    localStorage.setItem(name, replay.serialize());
+  } else {
+    localStorage.removeItem(name);
+  }
+}
+
+async function readFromStorage(name: string) {
+  const serialized = localStorage.getItem(name);
+  if (!serialized) {
+    return null;
+  }
+  const replay = Replay.deserialize(serialized);
+  const valid = replay && await replay.validate();
+  return valid ? replay : null;
+}
+
 export const actions: ActionsType<State, Actions> = {
   setPage: (page) => ({page}),
   setFullscreen: (fullscreen) => ({fullscreen}),
-  loadSave: (save) => (state) => (state.save === undefined ? {save} : null),
-  loadBest: (best) => (state) => (state.best === undefined ? {best} : null),
   clearSave: () => ({save: null}),
-  saveReplay: (replay) => null,
+  loadReplays: () => {
+    
+  },
+  saveReplay: (replay) => {
+  },
 };
