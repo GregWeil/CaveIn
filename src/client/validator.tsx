072b5450724dbfd7fbb4030907460e6d020c57d0
Glitch (cave-in) <none>
2018-09-01 03:27:59 +0000

    👞🎣 Checkpoint ./src/client/validator.tsx:97325/2568

--- src/client/validator.tsx
+++ src/client/validator.tsx
@@ -15,14 +15,34 @@ interface State {
 }
 
 interface Context {
-  validate(replay: Replay): boolean|null;
+  (replay: Replay): boolean|null;
 }
 
-const { Provider, Consumer } = createContext<Context>({validate
+const { Provider, Consumer } = createContext<Context>(() => null);
 
 export class ReplayValidator extends Component<Props, State> {
   state = {validated: new WeakMap()}
+  validate(replay: Replay) {
+    if (this.state.validated.has(replay)) {
+      return this.state.validated.get(replay);
+    }
+    this.setState((state: State) => {
+      const validated = state.validated;
+      validated.set(replay, null);
+      return {validated};
+    })
+    replay.validate().then(valid => {
+      this.setState((state: State) => {
+        const validated = state.validated;
+        validated.set(replay, valid);
+        return {validated};
+      });
+    });
+    return null;
+  }
   render({children}: Props, {validated}: State) {
-    return children;
+    return <Provider value={this.validate.bind(this)}>children</Provider>;
   }
 }
+
+export class
