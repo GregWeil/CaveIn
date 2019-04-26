424e3c19ce34d35ff7552d77cd7f75923cf6e572
Glitch (cave-in) <none>
2019-04-26 21:57:47 +0000

    ⚾️💴 Checkpoint ./package.json:97325/3664 ./src/client/layout.tsx:97325/21

--- src/client/layout.tsx
+++ src/client/layout.tsx
@@ -59,7 +59,7 @@ export class GameLayout extends Component<{children: ComponentChildren}, {scale:
 }
 
 interface GamePauserProps {
-  children: [(args: {paused: boolean, pause: () => void, resume: () => void}) => ComponentChild];
+  children: [(args: {paused: boolean, pause: () => void, resume: () => void}) => VNode];
 }
 
 export class GamePauser extends Component<GamePauserProps, {paused: boolean}> {
